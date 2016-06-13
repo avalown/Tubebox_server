99652562adab66e7f89e088c4be3b56af8477984
Jonathan <jonathan_cohen@outlook.com>
2016-06-13 22:38:06 +0200

    Maj socket de connexion initiale

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var swig = require('swig');
-var bson = require('bson');
-var monk = require('monk');
-var mongo = require('mongodb');
-var db = monk('localhost:27017/tubebox');
+var express = require('express')
+var http = require('http')
+var path = require('path')
+//var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var swig = require('swig')
+//var bson = require('bson')
+var monk = require('monk')
+var mongo = require('mongodb')
+var db = monk('localhost:27017/tubebox')
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index')
+var users = require('./routes/users')
 
-var app = express();
-var server = app.listen(8767);
-var io = require('socket.io').listen(server);
+var app = express()
+var server = app.listen(8767, "0.0.0.0")
+var io = require('socket.io').listen(server)
 
-io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !');   
-    socket.on('message', function (message) {
-        console.log('titre joué : ' + message);
-    });	
+io.on('connection', function (socket) {
+    console.log('New connection')
+
+    socket.on('new_session', function (code) {
+      console.log("New session")
+      socket.emit("status_response", "Je plaisantais :D")
+      console.log("Validation envoyée")
+    });
 });
 
 // view engine setup
 // utilisation du moteur de swig pour les .html
-app.engine('html', swig.renderFile); 
+app.engine('html', swig.renderFile);
 // utiliser le moteur de template pour les .html
-app.set('view engine', 'html'); 
+app.set('view engine', 'html');
 // dossier des vues
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname, 'views'));
 
 // view cache
 app.set('view cache', false); // désactivation du cache express
@@ -47,6 +50,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req,res,next){
     req.db = db;
     next();
+});
+
+// Enable Cross-Domain origin
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 app.use('/', routes);
